2bc6486245f18bb5bad4425a8f70e77059fd6670
Tyrone Tudehope <tyronetudehope@gmail.com>
2025-06-16 20:49:33 +0200

    prevent inventory calls for non inventory tracked items

--- src/elements/db/PurchasableQuery.php
+++ src/elements/db/PurchasableQuery.php
@@ -666,9 +666,9 @@ abstract class PurchasableQuery extends ElementQuery
             ->createCatalogPricesQuery(userId: $customerId)
             ->addSelect(['cp.purchasableId', 'cp.storeId']);
 
-        if (!empty($this->id)) {
+        if (!$this->id !== null) {
             $catalogPricesQuery->andWhere(['cp.purchasableId' => $this->id]);
-	}
+        }
 
         $this->subQuery->leftJoin(['sitestores' => Table::SITESTORES], '[[elements_sites.siteId]] = [[sitestores.siteId]]');
         $this->subQuery->leftJoin(['purchasables_stores' => Table::PURCHASABLES_STORES], '[[purchasables_stores.storeId]] = [[sitestores.storeId]] AND [[purchasables_stores.purchasableId]] = [[commerce_purchasables.id]]');
